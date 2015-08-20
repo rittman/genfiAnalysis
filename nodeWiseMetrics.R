82922356818d6785af18239293fb383031f21fd0
rittman <tr332@medschl.cam.ac.uk>
2015-08-20 22:23:31 +0100

    Added weighted measures and adjusted p-values. Plots significant values.

--- nodeWiseMetrics.R
+++ nodeWiseMetrics.R
@@ -4,6 +4,9 @@
 genfiDir = "/home/tim/GENFI/GENFI_camgrid_20150525/"
 setwd(genfiDir)
 
+library(xtable)
+library(ggplot2)
+
 #### helper functions ####
 nodeTtest <- function(vals, g1len, g2len){
   res <- t.test(vals[c(1:g1len)], vals[c((g1len+1):(g1len+g2len))])
@@ -26,26 +29,53 @@ nodeComparison <- function(dF){
                    dF[dF$GS==1,c(-1,-2,-3,-4)])
   gcs.len = length(gcs[,1])
   
+  # glue together control and gene carriers
   dF <- rbind(conts,gcs)
-  View(dF)
   
+  # remove any columns containing NA values
   dF <- dF[,apply(dF, 2, function(x) !any(is.na(x)))]
   
+  # do a t-test for each node
   resVals = apply(dF[,-1], 2, nodeTtest, g1len=conts.len, g2len=gcs.len)
-  resVals <- data.frame(matrix(unlist(resVals), byrow=TRUE, nrow=length(resVals)))
-  names(resVals) = c("t", "p")
+  resVals <- data.frame(matrix(unlist(resVals), byrow=TRUE, nrow=length(resVals)))  # reorganise values
+  names(resVals) = c("t", "p") # update column names
+  
+  # add node names and adjust p-values using Benjamini & Yekutieli (2001) method
   resVals <- data.frame(nodes=names(dF[,-1]),
                         resVals,
-                        p.adjust(resVals$p, method="BY"))
+                        p.adj=p.adjust(resVals$p, method="BY"))
   
+  # select only significant nodes (using corrected p-values)
+  res.plot <- resVals[resVals$p.adj<0.9,]
+
+  # carry on and plot if the number of rows is 0
+  if(nrow(res.plot)!=0){
+    print(res.plot)
+    res.plot <- data.frame(res.plot,
+                           contMean = sapply(res.plot$nodes, function(x) mean(dF[,x][c(1:conts.len)])),
+                           contGene = sapply(res.plot$nodes, function(x) mean(dF[,x][c(1:gcs.len)]))
+                           )
+    
+    res.plot <- res.plot[order(res.plot$contMean),] # reorder by the mean of hte control group
+    res.plot$nodes = factor(res.plot$nodes, levels=res.plot$nodes)
+    p <- ggplot(res.plot, aes_string(x="nodes",y="contMean"))
+    p <- p + geom_point()
+    View(res.plot)
+    ggsave("testPlot.png")
+  }
+  
+  # return the t-test results
   View(resVals)
   return(resVals)
 }
 
 #### run functions ####
 # import data
-gmList = list("d2_ccNorm_local",
-              "d2_elnNorm_local"
+gmList = list(#"d2_ccNorm_local",
+#               "d2_elnNorm_local",
+#               "d2_ccWt_local",
+              "d2_degree_wt_local",
+              "d2_degreeWt_local"
               )
 
 # select only a single percentage edge value
@@ -53,7 +83,10 @@ edgePC = 3
 
 for(gm in gmList){
   gm.dF <- read.table(gm, header=TRUE)
-  gm.dF <- gm.dF[gm.dF$"edgePC"==edgePC,]
+  if(!grepl("wt",gm)){ # check whether the graph metric is weighted
+    gm.dF <- gm.dF[gm.dF$"edgePC"==edgePC,] # if not weighted, then select only the specified edge percentage
+  }
+  
 #   print(apply(gm.dF, 2, function(x) !any(is.na(x))))
 #   gm.dF <- gm.dF[,apply(gm.dF, 2, function(x) !any(is.na(x)))]
   resVals = nodeComparison(gm.dF[,-1])
