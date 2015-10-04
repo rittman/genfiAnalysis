94d53b2bd3155c2814e6c28a75f51e7d92d6061d
rittman <tr332@medschl.cam.ac.uk>
2015-10-04 23:15:56 +0100

    Commenced work on breakpoint analysis. Bug fixes and added significance indicators for plots in wholeBrainMetrics.R.

---
+++ breakPointAnalysis.R
@@ -0,0 +1,46 @@
+library(bfast)
+
+smoothData <- function(GS, dF, bw=10){
+  if(!is.null(GS)){
+    dF <- dF[dF$GS==GS,]
+  } else {
+    GS="all gene positive"
+  }
+  
+  # resample data to enable time series analysis
+  aa = approxfun(dF$aoo, dF$geNorm)
+  dF.sm <- data.frame(x=seq(-50,50,by=.1), values=aa(seq(-50,50,by=0.1)))
+  dF.sm <- dF.sm[complete.cases(dF.sm),]
+  
+  # smooth using a kernel with specified bandwidth
+  dF.sm <- rbind(data.frame(dF.sm, type="orig"),
+                 data.frame(x=dF.sm$x,
+                            values=as.vector(ksmooth(dF.sm$x, dF.sm$values, bandwidth=bw))$y,
+                            type="smooth")
+                 )
+  dF.sm <- data.frame(dF.sm, GS=GS)
+  
+  return(dF.sm)
+}
+
+dF <- importGraphData("geNorm", FALSE, 3)
+# sp <- read.xlsx("../all_sm_thld10_SP.xlsx", sheetIndex = 1)
+dF <- importGraphData(metric, weighted, edgePC)
+# apply spike percentage threshold
+dF <- applySP(dF, sp)
+dF <- dF[dF$GS!="gene negative",]
+dF$GS <- droplevels(dF$GS)
+
+genfiData <- read.table("/home/tim/GENFI/genfi_Subjects_sjones_1_22_2015_17_47_47_restructure_summary.csv",
+sep="\t",
+header = TRUE)
+dF.aoo <- data.frame(wbic=genfiData$Subject,
+aoo=genfiData$Yrs.from.AV_AAO)
+dF <- merge(dF, dF.aoo, by="wbic")
+
+dF.sm <- do.call(rbind, lapply(levels(dF$GS), smoothData, dF=dF))
+dF.sm <- rbind(dF.sm, smoothData(NULL, dF))
+
+p <- ggplot(dF.sm, aes_string(x="x", y="values", colour="GS"))
+p <- p + geom_line(aes_string(linetype="type"))
+p <- p + geom_point(data=dF, aes_string(x="aoo", y="geNorm"), linetype="solid")
--- wholeBrainMetrics.R
+++ wholeBrainMetrics.R
@@ -131,12 +131,69 @@ cubicFun <- function(x, ac,bc,cc,dd){
   return(y)
 }
 
+addSigBar <- function(p, pVal, d1, d2, xList, sig.value=0.05){
+  if(pVal < sig.value){
+    ymax = ggplot_build(p)$panel$ranges[[1]]$y.range[2]
+    ymin = ggplot_build(p)$panel$ranges[[1]]$y.range[1]
+    ygap = ymax - ymin
+    
+    if(pVal < 0.0001){
+      pVal.plot = "****"
+    } else if(pVal < 0.001) {
+      pVal.plot = "***"
+    } else if(pVal < 0.01) {
+      pVal.plot = "**"
+    } else {
+      pVal.plot = "*"
+    }
+    ymax = ymax + .15*ygap
+    
+    ypos = ymax - ygap*.05
+    xpos1 = xList[d1]
+    xpos2 = xList[d2]
+    p <- p + geom_segment(x=xpos1, xend=xpos2, y=ypos, yend=ypos, colour="black")
+    p <- p + annotate("text", x=mean(c(xpos1, xpos2)), y=ypos, label=pVal.plot, colour="black", size=8)
+    p <- p + scale_y_continuous(limits=c(ymin, ymax))
+  }
+  return(p)
+}
+
+addSigBarGenes <- function(p, pVal, d1, d2, xList, ymax, ng, sig.value=0.05){
+  if(pVal < sig.value){
+    ymin = ggplot_build(p)$panel$ranges[[1]]$y.range[1]
+    ygap = ymax - ymin
+    
+    if(pVal < 0.0001){
+      pVal.plot = "****"
+    } else if(pVal < 0.001) {
+      pVal.plot = "***"
+    } else if(pVal < 0.01) {
+      pVal.plot = "**"
+    } else {
+      pVal.plot = "*"
+    }
+    ymax = ymax + .15*ygap
+    
+    ypos = ymax - ygap*.05
+    xpos1 = xList[d1]/3 + (ng-1) + 1/3 - (xList[d1]-2)*.08
+    
+    xpos2 = xList[d2]/3 + (ng-1) + 1/3 - (xList[d2]-2)*.08
+    
+    p <- p + geom_segment(x=xpos1, xend=xpos2, y=ypos, yend=ypos, colour="black")
+    p <- p + annotate("text", x=mean(c(xpos1, xpos2)), y=ypos, label=pVal.plot, colour="black", size=8)
+  }
+  return(list(p=p, ymax=ymax))
+}
+
+
 #### main functions ####
 wholeBrainAnalysis <- function(metric,
                                metricName,
+                               cols,
                                sp, weighted=TRUE,
                                outDir="wholeBrainResults",
-                               edgePC=3){
+                               edgePC=3, ts=12){
+  
   if(weighted){
     metric = paste(metric,"wt",sep="_")
   }
@@ -155,31 +212,28 @@ wholeBrainAnalysis <- function(metric,
   # import graph data
   dF <- importGraphData(metric, weighted, edgePC)
   
-  # Summarise the patient data
-  ptSum = ddply(dF, .(gene), summarise,
-                "gene negative" = length(GS[GS=="gene negative"]),
-                "gene positive" = length(GS[GS=="gene positive"]),
-                "affected"      = length(GS[GS=="affected"])
-  )
-  
-  print(xtable(ptSum,
-               caption="Subjects included in the analysis"),
-        include.rownames=FALSE,
-        file=outFile,
-        append=TRUE)
-  
   # apply spike percentage threshold
   dF <- applySP(dF, sp)
   
+  colList = unlist(cols[levels(dF$GS)])
+  
   # Summarise the patient data
   ptSum = ddply(dF, .(gene), summarise,
                 "gene negative" = length(GS[GS=="gene negative"]),
                 "gene positive" = length(GS[GS=="gene positive"]),
                 "affected"      = length(GS[GS=="affected"])
   )
+  ptSum <- data.frame(ptSum, Totals=rowSums(ptSum[,-1]))
+  tmpdF <- data.frame(gene="Totals",
+                      as.data.frame(matrix(colSums(ptSum[,-1]), nrow=1))
+  )
+  names(tmpdF) <- names(ptSum)
+  ptSum <- rbind(ptSum, tmpdF)
   
   print(xtable(ptSum,
-               caption="Subjects included in the analysis"),
+               caption="Subjects included in the analysis",
+               digits = c(0,0,0,0,0,0),
+               display = c("s", "s", "d", "d", "d","d")),
         include.rownames=FALSE,
         # file=outFile,
         append=TRUE)
@@ -237,16 +291,26 @@ wholeBrainAnalysis <- function(metric,
         file=outFile,
         append=TRUE)
         
-  
-  # now do a plot
+  # now do a plot for group differences, collapsed across genes
   p <- ggplot(dF.wb, aes_string(x="GS", y="values", fill="GS"))
   p <- p + geom_boxplot()
+  p <- p + scale_colour_manual(name="Group",values=as.vector(colList))
+  
+  xList = seq_along(levels(dF$GS))
+  names(xList) = levels(dF$GS)
+  for(n in seq(length(pwGS[1,]))){
+    pVal = as.numeric(as.character(pwTtests[n, "p"]))
+    d1 = pwGS[1,n]
+    d2 = pwGS[2,n]
+    p <- addSigBar(p, pVal, d1, d2, xList)
+  }
+  
   p <- p + theme_bw()
   p <- p + labs(y=metricName) + theme(axis.title.x=element_blank())
+  p <- p + theme(text=element_text(size=ts))
   ggsave(paste(outDir,
                paste(metric,"allgroups.png",sep="_"),
                sep="/"))
-  
   
   dF.wb.summary = ddply(dF.wb, .(gene), summarise,
                         "gene negative" = paste(sapply(mean(values[GS=="gene negative"], na.rm = TRUE), fn),
@@ -262,6 +326,10 @@ wholeBrainAnalysis <- function(metric,
         file=outFile,
         append=TRUE)
   
+  print(xtable(dF.wb.summary,
+               caption=paste("Mean and standard deviations for",metricName," values in individuals")),
+        append=TRUE)
+  
   # # plot this
   # err <- function(x) qnorm(0.975) * sd(x, na.rm = TRUE)/sqrt(length(x)) # function for standard error
   # 
@@ -272,16 +340,54 @@ wholeBrainAnalysis <- function(metric,
   
   # dF.stacked.plot <- data.frame(dF.stacked.plot, unique=seq(length(dF.stacked.plot[,1])))
   
+  # Do t-tests between diagnostic groups within each gene
+  dF.tResults <- data.frame()
+  for(gene in levels(dF.wb$gene)){
+    dF.temp <- dF.wb[dF.wb$gene==gene,]
+    pwTtests <- apply(pwGS, 2, function(x) dotTests(x,dF.temp))
+    pwTtests <- data.frame(gene=gene, matrix(unlist(pwTtests), nrow = length(pwTtests), byrow = TRUE))
+    names(pwTtests) <- c("gene", "comparison", "t", "p")
+    dF.tResults <- rbind(dF.tResults,
+                         pwTtests)
+  }
+  
+  # now do a plot for group differences for separate genes
   # p <- ggplot(dF.stacked.plot, aes_string(x="unique", y="valuesMean", middle="valuesMean", group="unique", fill="gene", upper="upper", lower="lower"))
-  p <- ggplot(dF.wb, aes_string(x="gene", y="values", fill="GS"))
-  p <- p + geom_boxplot()
-  p <- p + theme_bw()
-  p <- p + labs(y=metricName) + theme(axis.title.x=element_blank())
+  pg <- ggplot(dF.wb, aes_string(x="gene", y="values", fill="GS"))
+  pg <- pg + geom_boxplot()
+  pg <- pg + scale_colour_manual(name="Group",values=as.vector(colList))
+  
+  xList = seq_along(levels(dF$GS))
+  names(xList) = levels(dF$GS)
+  ymax = ggplot_build(pg)$panel$ranges[[1]]$y.range[2]
+  ymin = ggplot_build(pg)$panel$ranges[[1]]$y.range[1]
+  
+  ytop.max = ymax
+  for(ng in seq_along(levels(dF.wb$gene))){
+    ytop = ymax
+    gene = levels(dF.wb$gene)[ng]
+    for(n in seq(length(pwGS[1,]))){
+      pVal = as.numeric(as.character(dF.tResults[dF.tResults$gene==gene,][n, "p"]))
+      d1 = pwGS[1,n]
+      d2 = pwGS[2,n]
+      pL <- addSigBarGenes(pg, pVal, d1, d2, xList, ytop, ng)
+      pg = pL[["p"]]
+      ytop = pL[["ymax"]]
+    }
+    if(ytop>ytop.max){ ytop.max=ytop }
+  }
+  pg <- pg + scale_y_continuous(limits=c(ymin, ytop.max))
+  pg <- pg + theme_bw()
+  pg <- pg + labs(y=metricName) + theme(axis.title.x=element_blank())
+  pg <- pg + theme(text=element_text(size=ts))
+  
   ggsave(paste(outDir,
                paste(metric,"byGene.png",sep="_"),
                sep="/"))
   
   endLog(outFile)
+  
+  return(list(p,pg))
 }
 
 
@@ -294,7 +400,7 @@ graphTimeComparison <- function(metric,
                                weighted=TRUE, # is this a weighted metric?
                                outDir="wholeBrainVsAOOResults",
                                edgePC=3,
-                               h=30,w=45,s=4,
+                               h=30,w=45,s=4,ts=12,ps=4,
                                sink=TRUE){
   if(weighted){
     metric = paste(metric,"wt",sep="_")
@@ -386,12 +492,13 @@ graphTimeComparison <- function(metric,
   dF.plot <- dF
 
   p <- ggplot(dF.plot, aes_string(x="aoo", y="values", colour="GS", group="GS"))
-  p <- p + geom_point()
+  p <- p + geom_point(size=ps)
   pp <- p + geom_smooth(method="lm")
   colList = unlist(cols[levels(dF.plot$GS)])
-  pp <- pp + scale_colour_manual(values=as.vector(colList))
-  pp <- pp + labs(y=metricName, x="Estimated age of onset") + theme(axis.title.x=element_blank(), legend.key=element_rect(fill="white", colour="white"))
+  pp <- pp + scale_colour_manual(name="Group",values=as.vector(colList))
+  pp <- pp + labs(title=paste(metricName, "linear regression", sep="\n"), y=metricName, x="Estimated age of onset") + theme(axis.title.x=element_blank(), legend.key=element_rect(fill="white", colour="white"))
   pp <- pp + theme_bw() + theme(legend.key = element_rect(colour="#FFFFFF", fill = "#FFFFFF"))
+  pp <- pp + theme(text=element_text(size=ts), plot.title=element_text(size=ts))
   
   ggsave(paste(outDir,
                paste(metric,"png",sep="."),
@@ -406,7 +513,7 @@ graphTimeComparison <- function(metric,
   dF <- dF[dF$GS!="gene negative",]
   dF.plot <- dF
   p <- ggplot(dF.plot, aes_string(x="aoo", y="values", colour="GS", group="GS"))
-  p <- p + geom_point()
+  p <- p + geom_point(size=ps)
   
   # now run non-linear mixed effects model with a quadratic term
   nlOutFile = paste(outDir,
@@ -417,7 +524,6 @@ graphTimeComparison <- function(metric,
   }
   # firstly, plot the values to be able to estimate starting parameters
   if(is.null((startvec))){
-
     affirm = "n"
     while(affirm=="n"){
       print(pp)
@@ -430,20 +536,25 @@ graphTimeComparison <- function(metric,
       cq = as.numeric(readline(prompt = "quadratic multiplier: "))
       
       # dF.temp <- data.frame(x=dF$aoo, y=SSasymp(dF$aoo, Asym, R0, lrc), GS="Start estimates")
+      colList = unlist(cols[levels(dF.plot$GS)])
+      p.temp <- p.temp + scale_colour_manual(name="Group",values=as.vector(colList))
       dF.temp <- data.frame(x=dF$aoo, y=SSquadFun(dF$aoo, aq=aq, bq=bq, cq=cq), GS="Start estimates")
-      p.temp <- pp + geom_point(data=dF.temp, aes_string(x="x", y="y"))
+      p.temp <- p.temp + geom_point(data=dF.temp, aes_string(x="x", y="y"))
       print(p.temp)
       affirm = readline(prompt = "Are you happy with this?(y/n)")
     }
     # startvec = c(Asym=Asym, R0=R0, lrc=lrc)
     startvec = c(aq=aq, bq=bq, cq=cq)
   }
-  lapply(list("Starting values are as follows.",
-              paste("Constant:", startvec["aq"])),
-              paste("Multiplier of x term:", startvec["bq"]),
-              paste("Multiplier of quadratic term:", startvec["cq"]),
-         function(x) print(x)
-        )
+  
+  # save starting vectors for quadratic non-linear regression
+  qStartvec = startvec
+#   lapply(list("Starting values are as follows.",
+#               paste("Constant:", startvec["aq"]),
+#               paste("Multiplier of x term:", startvec["bq"]),
+#               paste("Multiplier of quadratic term:", startvec["cq"])),
+#          function(x) print(x)
+#         )
   
   # run non-linear quadratic model
   # dF <- cbind(dF, data.frame(aooNeg=dF$aoo*-1))  # make age of onset negative so that an asymptotic fit can be achieved.
@@ -463,10 +574,11 @@ graphTimeComparison <- function(metric,
   pq <- p + geom_line(data=dF.nlmq, aes_string(x="x", y="y"))
   cList = c("affected", "estimates", "gene positive")
   colList = unlist(cols[cList])
-  pq <- pq + scale_colour_manual(values=as.vector(colList),
+  pq <- pq + scale_colour_manual(name="Group",values=as.vector(colList),
                                  breaks=c("gene positive", "affected", "estimates"))
   pq <- pq + theme_bw() + theme(legend.key=element_rect(fill="white", colour="white"))
-  pq <- pq + labs(y=metricName, x="Estimated time from onset")# + theme(axis.title.x=element_blank())
+  pq <- pq + labs(title=paste(metricName, "non-linear regression, quadratic", sep="\n"), y=metricName, x="Estimated time from onset")# + theme(axis.title.x=element_blank())
+  pq <- pq + theme(text=element_text(size=ts), plot.title=element_text(size=ts), plot.title=element_text(size=ts))
   
 #   print(paste("Saving",paste(outDir,
 #                        paste(paste(metric, "nonLinearEstimatesQuadratic",sep="_"),"png",sep="."),
@@ -504,14 +616,15 @@ graphTimeComparison <- function(metric,
   }
   
   startvec = c(ac=ac, bc=bc, cc=cc, dc=dc)
+  cStartvec = startvec # save starting values for the cubic non-linear regression model
   
-  lapply(list("Starting values are as follows.",
-              paste("Constant:", startvec["ac"]),
-              paste("Multiplier of x term:", startvec["bc"]),
-              paste("Multiplier of quadratic term:", startvec["cc"]),
-              paste("Multiplier of cubic term:", startvec["dc"])),
-         function(x) print(x)
-  )
+#   lapply(list("Starting values are as follows.",
+#               paste("Constant:", startvec["ac"]),
+#               paste("Multiplier of x term:", startvec["bc"]),
+#               paste("Multiplier of quadratic term:", startvec["cc"]),
+#               paste("Multiplier of cubic term:", startvec["dc"])),
+#          function(x) print(x)
+#   )
   
   # run non-linear quadratic model
   # The following line calculates the non-linear model
@@ -530,10 +643,11 @@ graphTimeComparison <- function(metric,
   
   # pc <- ggplot()
   pc <- p + geom_line(data=dF.nlmc, aes_string(x="x", y="y"))
-  pc <- pc + scale_colour_manual(values=as.vector(colList),
+  pc <- pc + scale_colour_manual(name="Group",values=as.vector(colList),
                                  breaks=c("gene positive", "affected", "estimates"))
   pc <- pc + theme_bw() + theme(legend.key=element_rect(fill="white", colour="white"))
-  pc <- pc + labs(y=metricName, x="Estimated time from onset")# + theme(axis.title.x=element_blank())
+  pc <- pc + labs(title=paste(metricName, "non-linear regression, cubic", sep="\n"), y=metricName, x="Estimated time from onset")# + theme(axis.title.x=element_blank())
+  pc <- pc + theme(text=element_text(size=ts), plot.title=element_text(size=ts))
   
 #   print(paste("Saving",paste(outDir,
 #                              paste(paste(metric, "nonLinearEstimatesCubic",sep="_"),"png",sep="."),
@@ -554,7 +668,6 @@ graphTimeComparison <- function(metric,
   qvsc.anova = NULL
   try(
     qvsc.anova <- anova(nlmq, nlmc)
-    
   )
   
   if(!is.na(qvsc.anova)){
@@ -569,39 +682,46 @@ graphTimeComparison <- function(metric,
   
   # finalise log file and return dataframe
   endLog(outFile)
-  return(list(pp=pp, pq=pq, pc=pc))
+  return(list(list(pp=pp, pq=pq, pc=pc),
+              qStartvec,     # starting values for quadratic non-linear regression
+              cStartvec,     # starting values for cubic non-linear regression
+              summary(nlmq), # summary of non-linear quatric non-linear regression
+              summary(nlmc), # summary of non-linear cubic non-linear regression
+              qvsc.anova     # ANOVA between models with quadratic and cubic terms
+              )
+  )
 }
 
 
 # import spike percentage data
 # sp <- read.xlsx("../all_sm_thld10_SP.xlsx", sheetIndex = 1)
 
-metrics = list("degree"="connection strength",
-               "ge"="global efficiency",
-               "le"="local efficiency",
-               "pl"="path length",
-               "eigCentNP"="eigen centrality",
-               "betCent"="betweenness centrality",
-               "closeCent"="closeness centrality")
+# metrics = list("degree"="connection strength",
+#                "ge"="global efficiency",
+#                "le"="local efficiency",
+#                "pl"="path length",
+#                "eigCentNP"="eigen centrality",
+#                "betCent"="betweenness centrality",
+#                "closeCent"="closeness centrality")
 
 # lapply(names(metrics), function(x) wholeBrainAnalysis(x, metrics[[x]], sp))
 # lapply(names(metrics), function(x) graphTimeComparison(x, metrics[[x]], sp))
 
 # do metrics for unweighted graphs
-metrics = list("degree"="connection strength",
-               "degreeNorm"="connection strength (normalised)",
-               "ge"="global efficiency",
-               "geNorm"="global efficiency (normalised)",
-               "le"="local efficiency",
-               "leNorm"="local efficiency (normalised)",
-               "pl"="path length",
-               "plNorm"="path length (normalised)",
-               "eigCentNP"="eigen centrality",
-               "eigCentNorm"="eigen centrality (normalised)",
-               "betCent"="betweenness centrality",
-               "betCentNorm"="betweenness centrality (normalised)",
-               "closeCent"="closeness centrality",
-               "closeCentNorm"="closeness centrality (normalised)")
+# metrics = list("degree"="connection strength",
+#                "degreeNorm"="connection strength (normalised)",
+#                "ge"="global efficiency",
+#                "geNorm"="global efficiency (normalised)",
+#                "le"="local efficiency",
+#                "leNorm"="local efficiency (normalised)",
+#                "pl"="path length",
+#                "plNorm"="path length (normalised)",
+#                "eigCentNP"="eigen centrality",
+#                "eigCentNorm"="eigen centrality (normalised)",
+#                "betCent"="betweenness centrality",
+#                "betCentNorm"="betweenness centrality (normalised)",
+#                "closeCent"="closeness centrality",
+#                "closeCentNorm"="closeness centrality (normalised)")
 
 # lapply(names(metrics), function(x) wholeBrainAnalysis(x, metrics[[x]], sp, weighted=FALSE))
 
