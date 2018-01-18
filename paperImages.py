09f3bb36403cc558974eb2242b789fd11baf6f9e
Tim Rittman <tr332@medschl.cam.ac.uk>
2018-01-18 23:15:32 +0000

    Combined figures 1 and 2.

--- paperImages.py
+++ paperImages.py
@@ -9,6 +9,7 @@ from PIL import Image
 # path definitions
 out_directory = "figures"
 wbvsaoo = "wholeBrainVsAOOResults"
+wbvsaoo_lobar = "Reports/lobar_reports/wholeBrainVsAOOResults"
 wbr_reports = "Reports/wholeBrainResults"
 wbr = "wholeBrainResults"
 wbr_lobes = "Reports/lobar_reports/wholeBrainResults"
@@ -47,37 +48,85 @@ def importResize(inList, outwd, ppmm):
         hList.append(float(h_in)) # append length to list
         imgList_in.append(im_in) # append image to list
 
-    # get ratio - nb only works with two images
+    
+    # output height
+    max_h = max(hList)
+    max_h_n = hList.index(max_h)
+    
+    for n,input_image in enumerate(imgList_in):
+        wList[n]*=(max_h/hList[n])
+        hList[n]*=(max_h/hList[n])
+    # get ratio
     # multiplier for the first image
-    y_r = ((hList[1]*outwd) / ((wList[0]*hList[1]) + (hList[0]*wList[1])))
-    x_r = ((y_r*hList[0]) / hList[1]) # multiplier for the second image
+    total_width_input = float(sum(wList))
 
-    w0 = int(y_r*wList[0]) # new width for first image
-    h0 = int(y_r*hList[0]) # new height for first image
-
-    w1_a = int(x_r*wList[1]) # new width for second image
-    h1_a = int(x_r*hList[1]) # new height for second image
-
-    im0_in = imgList_in[0].resize((w0, h0)) # resize first image
-    im1_in = imgList_in[1].resize((w1_a, h1_a)) # resize second image
-
-    outIm = Image.new("RGB", (outwd, h0), "white") # create the output image
-
-    # paste the first image
+#    out_h = int((float(outwd)/float(wList[max_h_n])) * max_h)
+    out_h = int(max_h * (outwd/(total_width_input/float(wList[max_h_n]))/wList[max_h_n]))
+    print([outwd,out_h])
+    
+    outIm = Image.new("RGB", (outwd, out_h), "white") # create the output image
+    
     left_a = 0
     upper_a = 0
-    right_a = w0
-    lower_a = h0
-
-    outIm.paste(im0_in, (left_a, upper_a, right_a, lower_a))
-
-    # paste the first image
-    left_b = right_a
-    upper_b = 0
-    right_b = left_b+w1_a
-    lower_b = h1_a
-
-    outIm.paste(im1_in, (left_b, upper_b, right_b, lower_b))
+    
+    
+    for n,input_image in enumerate(imgList_in):
+        # percentage of total width
+        w_pc = float(wList[n])/total_width_input
+        
+        # output width of panel
+        outwd_panel = int(int(outwd) * w_pc)
+        
+        # output panel height
+        outh_panel = int((float(outwd_panel)/float(wList[n]))*hList[n])
+       
+        
+        
+    
+        # resize the image
+        im_in = imgList_in[n].resize((outwd_panel,outh_panel))
+        
+        left_b = left_a
+        upper_b = upper_a
+        right_b = left_b+outwd_panel
+        lower_b = upper_b+outh_panel
+        
+        outIm.paste(im_in, (left_b, upper_b, right_b, lower_b))
+        
+        left_a = right_b
+        
+    
+#    y_r = ((hList[1]*outwd) / ((wList[0]*hList[1]) + (hList[0]*wList[1])))
+#    
+#    
+#    x_r = ((y_r*hList[0]) / hList[1]) # multiplier for the second image
+#
+#    w0 = int(y_r*wList[0]) # new width for first image
+#    h0 = int(y_r*hList[0]) # new height for first image
+#
+#    w1_a = int(x_r*wList[1]) # new width for second image
+#    h1_a = int(x_r*hList[1]) # new height for second image
+#
+#    im0_in = imgList_in[0].resize((w0, h0)) # resize first image
+#    im1_in = imgList_in[1].resize((w1_a, h1_a)) # resize second image
+#
+#    outIm = Image.new("RGB", (outwd, h0), "white") # create the output image
+#
+#    # paste the first image
+#    left_a = 0
+#    upper_a = 0
+#    right_a = w0
+#    lower_a = h0
+#
+#    outIm.paste(im0_in, (left_a, upper_a, right_a, lower_a))
+#
+#    # paste the first image
+#    left_b = right_a
+#    upper_b = 0
+#    right_b = left_b+w1_a
+#    lower_b = h1_a
+#
+#    outIm.paste(im1_in, (left_b, upper_b, right_b, lower_b))
 
     return outIm # return the output image
 
@@ -85,15 +134,18 @@ def importResize(inList, outwd, ppmm):
 img1 = path.join(wbr_reports, "degree_wt_allgroups.png")
 #img2 = path.join(wbvsaoo, "degree_wtGenePos.png")
 img2 = path.join(wbr_reports, "degree_wt_byGene.png")
+ 
+img3 = path.join(wbvsaoo_lobar, "degree_wtGenePos_DiscontBkpt.png")
 
-outImgTop = importResize([img1, img2], c1w, ppm)
+outImgTop = importResize([img1, img2, img3], c1w, ppm)
 
 # Image 1 - bottom panel
 img1 = path.join(wbr_reports, "geNorm_allgroups.png")
 #img2 = path.join(wbvsaoo, "geNormGenePos.png")
 img2 = path.join(wbr_reports, "geNorm_byGene.png")
+img3 = path.join(wbvsaoo_lobar, "geNormGenePos_DiscontBkpt.png")
 
-outImgBottom = importResize([img1, img2], c1w, ppm)
+outImgBottom = importResize([img1, img2, img3], c1w, ppm)
 
 # get the heights of the top and bottom panel
 wt, ht = outImgTop.size
@@ -121,8 +173,8 @@ outImg.info["dpi"] = dpi
 outImg.save(path.join(out_directory, "Figure1.png"))
 
 ### Figure 4 - breakpoint analysis
-img1 = path.join(wbvsaoo, "degree_wtGenePos_DiscontBkpt_.png")
-img2 = path.join(wbvsaoo, "geNormGenePos_DiscontBkpt_.png")
+img1 = path.join(wbvsaoo_lobar, "degree_wtGenePos_DiscontBkpt.png")
+img2 = path.join(wbvsaoo_lobar, "geNormGenePos_DiscontBkpt.png")
 
 im1 = Image.open(img1)
 w1, h1 = im1.size
